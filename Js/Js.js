85c3756acf8a1eb54f5718e6382d99b389fdac9d
Amr Elbardini <49646105+amrelbardini@users.noreply.github.com>
2020-02-11 18:07:25 +0200

    Template-5
    Responsive Template using BootStrap latest version(v.4.4.1)
    
    For Educational purposes.(ElZeroWebSchool)

---
+++ Js/Js.js
@@ -0,0 +1,33 @@
+﻿$(function ()
+{
+    'use strict'
+
+
+    //adjust the slider's height using Js
+    var windowHeight = $(window).height(),
+        NavHeight = $(".navbar").innerHeight(), // to get the height including padding and margin values
+        UpperBar = $(".upper-bar").innerHeight();
+
+    $(".slider .carousel-item , .overlay").height(windowHeight - (NavHeight + UpperBar));
+
+    // active in nav bar
+    $(".navbar .collapse ul li").on("click", function ()
+    {
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+
+
+    // click to shuffle class
+    $(".featured-work  ul > li").on("click",function ()
+    {
+        $(this).addClass("active").siblings().removeClass("active");
+        if ($(this).data("class") === "all") {
+            $(".images-container .col-md").css("opacity",1);
+        } else {
+            $(".images-container .col-md").css("opacity", .09);
+            $($(this).data("class")).parent().css("opacity", 1);
+            console.log($($(this).data("class")));
+        }
+    });
+    
+});
